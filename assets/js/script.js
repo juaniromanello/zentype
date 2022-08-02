ff5816cf82d1d383f5e3bca7c7fa5dc8ed772a45
ROMANELLO Juanita <juanitaromanello@gmail.com>
2022-08-02 19:59:56 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+
+function valor_navbar(){
+    let navbar=document.getElementById("navbar").value;
+
+    if (navbar == "navbar"){
+        document.getElementById("htmlNav").classList.remove("hide");
+        document.getElementById("htmlNav").classList.add("show");
+        document.getElementById("navbar-html").classList.add("hide");
+        document.getElementById("hero-html").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+function valor_hero(){
+    let hero=document.getElementById("hero").value;
+
+    if (hero == "hero"){
+        document.getElementById("htmlHero").classList.remove("hide");
+        document.getElementById("htmlHero").classList.add("show");
+        document.getElementById("hero-html").classList.add("hide");
+        document.getElementById("quienes-html").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+function valor_quienes(){
+    let quienes=document.getElementById("quienes").value;
+
+    if (quienes == "quienes"){
+        document.getElementById("htmlQuienes").classList.remove("hide");
+        document.getElementById("htmlQuienes").classList.add("show");
+        document.getElementById("quienes-html").classList.add("hide");
+        document.getElementById("serv-html").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+function valor_serv(){
+    let serv=document.getElementById("serv").value;
+
+    if (serv == "servicios"){
+        document.getElementById("htmlServ").classList.remove("hide");
+        document.getElementById("htmlServ").classList.add("show");
+        document.getElementById("serv-html").classList.add("hide");
+        document.getElementById("a-html").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+
+
+
+
+
