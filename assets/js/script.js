8e9c196e240bd95a3c983e1ea092a08f33164cba
ROMANELLO Juanita <juanitaromanello@gmail.com>
2022-08-03 21:52:57 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
+/* HTML */
 
 function valor_navbar(){
     let navbar=document.getElementById("navbar").value;
 
     if (navbar == "navbar"){
         document.getElementById("htmlNav").classList.remove("hide");
-        document.getElementById("htmlNav").classList.add("show");
         document.getElementById("navbar-html").classList.add("hide");
         document.getElementById("hero-html").classList.remove("hide");
     } else {
@@ -17,7 +17,6 @@ function valor_hero(){
 
     if (hero == "hero"){
         document.getElementById("htmlHero").classList.remove("hide");
-        document.getElementById("htmlHero").classList.add("show");
         document.getElementById("hero-html").classList.add("hide");
         document.getElementById("quienes-html").classList.remove("hide");
     } else {
@@ -30,7 +29,6 @@ function valor_quienes(){
 
     if (quienes == "quienes"){
         document.getElementById("htmlQuienes").classList.remove("hide");
-        document.getElementById("htmlQuienes").classList.add("show");
         document.getElementById("quienes-html").classList.add("hide");
         document.getElementById("serv-html").classList.remove("hide");
     } else {
@@ -43,9 +41,71 @@ function valor_serv(){
 
     if (serv == "servicios"){
         document.getElementById("htmlServ").classList.remove("hide");
-        document.getElementById("htmlServ").classList.add("show");
         document.getElementById("serv-html").classList.add("hide");
-        document.getElementById("end-html").classList.remove("hide");
+        document.getElementById("port-html").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+function valor_port(){
+    let port=document.getElementById("port").value;
+
+    if (port == "portfolio"){
+        document.getElementById("htmlPortfolio").classList.remove("hide");
+        document.getElementById("port-html").classList.add("hide");
+        document.getElementById("cont-html").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+function valor_cont(){
+    let cont=document.getElementById("cont").value;
+
+    if (cont == "contacto"){
+        document.getElementById("htmlContact").classList.remove("hide");
+        document.getElementById("cont-html").classList.add("hide");
+        document.getElementById("video-html1").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+/* VIDEO SIZE */
+
+function video_size1(){
+    let video=document.getElementById("video").value;
+
+    if (video == "video"){
+        document.getElementById("video-html1").classList.add("hide");
+        document.getElementById("css-html").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+/* LINK CSS */
+
+function css_link(){
+    let css=document.getElementById("css").value;
+
+    if (css == "styles.css"){
+        document.getElementById("css-html").classList.add("hide");
+        document.getElementById("video-html2").classList.remove("hide");
+    } else {
+        alert("El código está mal");
+    }
+}
+
+/* VIDEO SIZE 2 */
+
+function video_size2(){
+    let video=document.getElementById("video").value;
+
+    if (video == "video"){
+        document.getElementById("video-html2").classList.add("hide");
+        document.getElementById("hero-video").classList.add("video-size");
     } else {
         alert("El código está mal");
     }
